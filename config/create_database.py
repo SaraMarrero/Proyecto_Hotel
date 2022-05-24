e40ac4cda274f104a3b967b4976a69486476d2ef
Alejandro <alejandrodominguezc7@gmail.com>
2022-05-24 23:35:33 +0100

    Arreglo de errores en la base de datos

--- config/create_database.py
+++ config/create_database.py
@@ -18,40 +18,46 @@ def create_database(db_file):
 
     con.execute("CREATE TABLE recepcionista (id_recepcionista int PRIMARY KEY, nombre varchar(10), apellido1 varchar(15), apellido2 varchar(15))")
 
-    con.execute("INSERT INTO recepcionista VALUES ((1,'Francisco','Trujillo','Ojeda'), (2,'Monica','Naranjo','Carrasco'), (3,'Natalia','Pulido','Suarez'), (4,'Gisela','Rodriguez','Garcia'), (5,'Juanjo','Sarmiento','Gonzalez'), (6,'Luis','Santos','Alonso'))")
+    con.execute("INSERT INTO recepcionista VALUES (1,'Francisco','Trujillo','Ojeda'), (2,'Monica','Naranjo','Carrasco'), (3,'Natalia','Pulido','Suarez'), (4,'Gisela','Rodriguez','Garcia'), (5,'Juanjo','Sarmiento','Gonzalez'), (6,'Luis','Santos','Alonso')")
 
 
 
     con.execute("CREATE TABLE reservas (id_reserva int primary key, fecha_entrada date, fecha_salida date)")
 
-    con.execute("INSERT INTO recepcionista VALUES ((1,'2018-10-14','2018-10-17'), (2,'2017-08-30','2017-09-06'), (3,'2019-11-30','2019-12-04'), (4,'2017-06-08','2017-06-20'), (5,'2018-01-17','2018-01-20'), (6,'2019-09-19','2019-09-29'), (7,'2017-01-20','2017-01-24'), (8,'2018-05-23','2018-05-26'), (9,'2019-04-19','2019-04-23'), (10,'2017-08-26','2017-08-29'))")
+    con.execute("INSERT INTO reservas VALUES (1,'2018-10-14','2018-10-17'), (2,'2017-08-30','2017-09-06'), (3,'2019-11-30','2019-12-04'), (4,'2017-06-08','2017-06-20'), (5,'2018-01-17','2018-01-20'), (6,'2019-09-19','2019-09-29'), (7,'2017-01-20','2017-01-24'), (8,'2018-05-23','2018-05-26'), (9,'2019-04-19','2019-04-23'), (10,'2017-08-26','2017-08-29')")
 
 
 
     con.execute("CREATE TABLE servicios (id_servicio int primary key, tipo_servicio varchar(30), precio int)")
 
-    con.execute("INSERT INTO recepcionista VALUES ((1,'servicio de limpieza',20), (2,'servicio de transporte',12), (3,'servicio de excursiones',25), (4,'servicio de guia-traductor',30), (5,'servicio de spa-talasoterapia',50))")
+    con.execute("INSERT INTO servicios VALUES (1,'servicio de limpieza',20), (2,'servicio de transporte',12), (3,'servicio de excursiones',25), (4,'servicio de guia-traductor',30), (5,'servicio de spa-talasoterapia',50)")
 
 
 
     con.execute("CREATE TABLE recepcionista_cliente_reservas (id_recepcionista int, dni_cliente varchar(9), id_reserva int, primary key(id_recepcionista, dni_cliente, id_reserva))")
 
-    con.execute("INSERT INTO recepcionista_cliente_reservas VALUES ((1,'11111111A',1), (2,'11111111B',2), (3,'11111111C',3), (4,'11111111D',4), (5,'11111111E',5), (6,'11111111F',6), (1,'11111111G',7), (2,'11111111H',8), (3,'11111111I',9), (4,'11111111J',10))")
+    con.execute("INSERT INTO recepcionista_cliente_reservas VALUES (1,'11111111A',1), (2,'11111111B',2), (3,'11111111C',3), (4,'11111111D',4), (5,'11111111E',5), (6,'11111111F',6), (1,'11111111G',7), (2,'11111111H',8), (3,'11111111I',9), (4,'11111111J',10)")
 
 
 
     con.execute("CREATE TABLE reservas_habitacion (id_reserva int, numero_habitacion int, primary key(id_reserva, numero_habitacion))")
 
-    con.execute("INSERT INTO reservas_habitacion VALUES ((1,236), (2,512), (3,189), (4,67), (5,287), (6,162), (7,517), (8,24), (9,436), (10,14))")
+    con.execute("INSERT INTO reservas_habitacion VALUES (1,236), (2,512), (3,189), (4,67), (5,287), (6,162), (7,517), (8,24), (9,436), (10,14)")
 
 
 
     con.execute("CREATE TABLE reservas_servicios (id_reserva int, id_servicio int, primary key(id_reserva,id_servicio))")
 
-    con.execute("INSERT INTO reservas_servicios VALUES ((1,2), (2,4), (3,3), (4,4), (5,1), (6,3), (7,5), (8,2), (9,4), (10,5))")
+    con.execute("INSERT INTO reservas_servicios VALUES (1,2), (2,4), (3,3), (4,4), (5,1), (6,3), (7,5), (8,2), (9,4), (10,5)")
 
 
-    con.execute("alter table recepcionista_cliente_reservas add foreign key (id_recepcionista) references recepcionista(id_recepcionista))")
+    "alter table recepcionista_cliente_reservas add foreign key (id_recepcionista) references recepcionista(id_recepcionista)"
+    "alter table recepcionista_cliente_reservas add foreign key (dni_cliente) references cliente(dni)"
+    "alter table recepcionista_cliente_reservas add foreign key (id_reserva) references reservas(id_reserva)"
+    "alter table reservas_habitacion add foreign key (id_reserva) references reservas(id_reserva)"
+    "alter table reservas_habitacion add foreign key (numero_habitacion) references habitaciones(numero_habitacion)"
+    "alter table reservas_servicios add foreign key (id_reserva) references reservas(id_reserva)"
+    "alter table reservas_servicios add foreign key (id_servicio) references servicios(id_servicio)"
 
 
     con.commit()
