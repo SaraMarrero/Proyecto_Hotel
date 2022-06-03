a71e1611431bfdedad6c6440f7c6c0fed24554b3
SaraMarrero <saramarreromiranda@gmail.com>
2022-06-03 14:03:53 +0100

    Avance aplicación

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/saramarrero/Desktop/Proyecto/.venv"
+set -gx VIRTUAL_ENV "/Users/saramarrero/Documents/GitHub/Proyecto_Hotel/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
---
+++ config/config.py
@@ -0,0 +1,1 @@
+DATABASE = 'datos.db'
